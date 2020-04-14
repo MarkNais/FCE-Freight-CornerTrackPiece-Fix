066c12e21edf107f66ba88c58eab23dc85c136a9
MarkNais <MarkNaismith@Outlook.com>
2020-04-13 20:10:53 -0700

    Setup

---
+++ CornerTrackPiece-Fix/CornerTrackPieceFix.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace CornerTrackPiece_Fix
+{
+
+    public class FreightCornerFix : FortressCraftMod
+    {
+        public override ModRegistrationData Register()
+        {
+            ModRegistrationData registrationData = new ModRegistrationData();
+
+            var harmony = HarmonyInstance.Create("MarkNstein.FreightCornerFix");
+            HarmonyInstance.DEBUG = true;
+            //FileLog.Reset(); //clears output from other mods
+            FileLog.Log("MarkNstein.FreightCornerFix registered. Begin Harmony");
+            Debug.Log("MarkNstein.FreightCornerFix registered. Begin Harmony");
+
+            try
+            {
+                harmony.PatchAll();
+            }
+            catch (Exception e)
+            {
+                FileLog.Log("Caught exception when running patcher!");
+                FileLog.Log(e.Message);
+                FileLog.Log(e.StackTrace);
+                FileLog.Log(e.ToString());
+            }
+
+            return registrationData;
+        }
+
+    }
+}
---
+++ CornerTrackPiece-Fix/RoomEnvironmentalsMod.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;  //Needed for most Unity Enginer manipulations: Vectors, GameObjects, Audio, etc.
+using System.IO;    //For data read/write methods
+using System;    //For data read/write methods
+using System.Collections.Generic;   //Working with Lists and Collections
+using System.Linq;   //More advanced manipulation of lists/collections
+using Harmony;
+using ReikaKalseki;
+
+public class FreightCornerFix : FortressCraftMod
+	{
+	public override ModRegistrationData Register()
+	{
+		ModRegistrationData registrationData = new ModRegistrationData();
+
+		var harmony = HarmonyInstance.Create("MarkNstein.FreightCornerFix");
+		HarmonyInstance.DEBUG = true;
+		//FileLog.Reset(); //clears output from other mods
+		FileLog.Log("MarkNstein.FreightCornerFix registered. Begin Harmony");
+		Debug.Log("MarkNstein.FreightCornerFix registered. Begin Harmony");
+
+		try {
+			harmony.PatchAll();
+		}
+		catch (Exception e) {
+			FileLog.Log("Caught exception when running patcher!");
+			FileLog.Log(e.Message);
+			FileLog.Log(e.StackTrace);
+			FileLog.Log(e.ToString());
+		}
+		
+		return registrationData;
+	}
+
+}
